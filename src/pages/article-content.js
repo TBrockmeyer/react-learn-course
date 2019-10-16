1d2757d6bf28466d18738167d2507190973be89c
t.brockmeyer <t.brockmeyer@criteo.com>
2019-10-16 10:22:49 +0200

    Initial commit

---
+++ src/pages/article-content.js
@@ -0,0 +1,90 @@
+const articles = [
+    {
+        name: 'learn-react',
+        title: 'The Fastest Way to Learn React',
+        content: [
+            `Welcome! Today we're going to be talking about the fastest way to
+            learn React. We'll be discussing some topics such as proin congue
+            ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
+            Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
+            non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
+            eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
+            sodales purus euismod.`,
+            `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+            `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+        ]
+    },    {
+        name: 'learn-node',
+        title: 'How to Build a Node Server in 10 Minutes',
+        content: [
+            `In this article, we're going to be talking looking at a very quick way
+            to set up a Node.js server. We'll be discussing some topics such as proin congue
+            ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
+            Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
+            non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
+            eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
+            sodales purus euismod.`,
+            `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+            `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+        ]
+    },     {
+        name: 'my-thoughts-on-resumes',
+        title: 'My Thoughts on Resumes',
+        content: [
+            `Today is the day I talk about something which scares most people: resumes.
+            In reality, I'm not sure why people have such a hard time with proin congue
+            ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
+            Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
+            non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
+            eleifend ut ipsum non fringilla. Praesent imperdiet nulla nec est luctus, at 
+            sodales purus euismod.`,
+            `Donec vel mauris lectus. Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+            `Etiam nec lectus urna. Sed sodales ultrices dapibus. 
+            Nam blandit tristique risus, eget accumsan nisl interdum eu. Aenean ac accumsan 
+            nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
+            sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
+            interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
+            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+        ]
+    },     {
+        name: 'learn-to-code',
+        title: 'Learn to code in 2019, get hired, and have fun',
+        content: [
+            `You are going to become a Javascript ninja/ninjess for the following reasons:
+			→ Javascript is everywhere. Every company that has a website or an app needs someone with Javascript knowledge. This language is a requirement for a TON of job postings (If you don’t trust me, search for Javascript in your area on LinkedIn).
+			→ With the introduction of Node.js, you can use Javascript to create a full-stack app (english = you can use javascript to build your entire project). Using tools like Electron, React Native and many others, Javascript allows you to build a desktop app, a mobile app, a web app, and even VR apps. You can control robots by using something like Jonny-Five. You can build your own blockchain. Lisk, an ethereum like platform uses javascript. Want to use AI and Machine Learning? Great, you can use Javascript to do all of that too.
+			→ If you didn’t read the first point and you were thinking about something else, let me reiterate: Industry demand for Javascript experts is HUGE. It is the most in demand language by employers. It is not uncommon for developers to get contacted by recruiters and head hunters multiple times a week for job offers.
+			→ Javascript community is growing at a crazy fast pace. There is a lot of new developments in the community. Some people complain about Javascript fatigue, since more and more new tools are being developed every day. However, we can use this to our advantage. You would be learning these new technologies at the same time as people who have been in the industry for years.
+			Trust me, it is a great community with a lot of demand. If you don’t trust me, here is a trending developer skills analysis.
+			Enough jabber, let’s get started. Below you will find what I believe are the best for you to get the most out of your time. By the end of 5 months, you should be able to land your first real non-entry level programming job. No bootcamps. Just you and your determination.
+`,
+        ]
+    },    
+];
+
+export default articles;
